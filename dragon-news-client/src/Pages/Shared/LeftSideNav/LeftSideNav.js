51b0b47a32583d914225bd1509e6ea38c4550a35
RaisulislamRimon <rimonppip0@gmail.com>
2022-10-19 04:51:53 +0600

    cors policy error

--- dragon-news-client/src/Pages/Shared/LeftSideNav/LeftSideNav.js
+++ dragon-news-client/src/Pages/Shared/LeftSideNav/LeftSideNav.js
@@ -1,7 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const LeftSideNav = () => {
   const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/news-categories`)
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error(error));
+  }, []);
   return (
     <div>
       <h4>All Category</h4>
