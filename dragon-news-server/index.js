74799a842e3c013ab1682d6e8ff47432baadae96
RaisulislamRimon <rimonppip0@gmail.com>
2022-10-18 23:02:14 +0600

    server init

--- dragon-news-server/index.js
+++ dragon-news-server/index.js
@@ -2,3 +2,11 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 // const port = 5000;
+
+app.get("/", (req, res) => {
+  res.send("NEWS API RUNNING");
+});
+
+app.listen(port, () => {
+  console.log(`Dragon News Server running on port `, port);
+});
