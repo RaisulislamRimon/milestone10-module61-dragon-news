ae096636f15fd159f7bdc8d06288d878560182e8
RaisulislamRimon <rimonppip0@gmail.com>
2022-10-20 01:34:21 +0600

    update server

--- dragon-news-server/index.js
+++ dragon-news-server/index.js
@@ -8,6 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require("./data/categories.json");
+const news = require("./data/news.json");
 
 app.get("/", (req, res) => {
   res.send("NEWS API RUNNING");
@@ -15,6 +16,18 @@ app.get("/", (req, res) => {
 
 app.get("/news-categories", (req, res) => {
   res.send(categories);
+});
+
+app.get("/news-categories/:id", (req, res) => {
+  const id = req.params.id;
+  const category_news = news.filter((n) => n.category_id === id);
+  res.send(category_news);
+});
+
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find((n) => n._id === id);
+  res.send(selectedNews);
 });
 
 app.listen(port, () => {
