3973b3a3f6b4040510145d0482e5fe4f9d955fe1
RaisulislamRimon <rimonppip0@gmail.com>
2022-10-21 00:03:03 +0600

    update

--- dragon-news-client/src/Pages/Login/Register/Register.js
+++ dragon-news-client/src/Pages/Login/Register/Register.js
@@ -6,16 +6,19 @@ const Register = () => {
   const handleSubmit = (event) => {
     event.preventDefault();
     const form = event.target;
+    const name = form.name.value;
+    const photoURL = form.photoURL.value;
     const email = form.email.value;
     const password = form.password.value;
+    console.log(name, photoURL, email, password);
   };
   return (
     <Form onSubmit={handleSubmit}>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
+      <Form.Group className="mb-3" controlId="formBasicName">
         <Form.Label>Your Name </Form.Label>
         <Form.Control type="text" name="name" placeholder="Enter name" />
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
+      <Form.Group className="mb-3" controlId="formBasicPhotoUrl">
         <Form.Label>Photo URL</Form.Label>
         <Form.Control
           type="text"
