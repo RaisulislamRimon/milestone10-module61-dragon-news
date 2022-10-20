1cdca97046bcdb0841beebfb8b06c517b5acbcb9
RaisulislamRimon <rimonppip0@gmail.com>
2022-10-20 18:15:14 +0600

    update card

--- dragon-news-client/src/Pages/Shared/NewsSummaryCard/NewsSummaryCard.js
+++ dragon-news-client/src/Pages/Shared/NewsSummaryCard/NewsSummaryCard.js
@@ -2,14 +2,32 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import { Link } from "react-router-dom";
+import Image from "react-bootstrap/Image";
+import { FaRegBookmark, FaShareAlt } from "react-icons/fa";
 
 const NewsSummaryCard = ({ news }) => {
   console.log(news);
   const { _id, title, total_views, author, details, image_url } = news;
   return (
     <div>
-      <Card className="">
-        <Card.Header>Featured</Card.Header>
+      <Card className="mb-5">
+        <Card.Header className="d-flex justify-content-between align-items-center">
+          <div className="d-flex">
+            <Image
+              roundedCircle
+              src={author.img}
+              style={{ height: "60px" }}
+            ></Image>
+            <div className=" ms-3">
+              <p>{author.name}</p>
+              <p>{author.published_date}</p>
+            </div>
+          </div>
+          <div>
+            <FaRegBookmark />
+            <FaShareAlt />
+          </div>
+        </Card.Header>
         <Card.Body>
           <Card.Title>{title}</Card.Title>
           <Card.Img variant="top" src={image_url} />
