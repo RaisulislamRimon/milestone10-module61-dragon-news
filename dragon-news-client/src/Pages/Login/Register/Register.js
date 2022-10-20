c29ec569fec8011eef920200e4d3f6d9015480bc
RaisulislamRimon <rimonppip0@gmail.com>
2022-10-20 23:48:42 +0600

    Create Login and Registration Form

--- dragon-news-client/src/Pages/Login/Register/Register.js
+++ dragon-news-client/src/Pages/Login/Register/Register.js
@@ -3,8 +3,14 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
 const Register = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+  };
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Your Name </Form.Label>
         <Form.Control type="text" name="name" placeholder="Enter name" />
@@ -19,12 +25,22 @@ const Register = () => {
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" name="email" placeholder="Enter email" />
+        <Form.Control
+          type="email"
+          name="email"
+          placeholder="Enter email"
+          required
+        />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" name="password" placeholder="Password" />
+        <Form.Control
+          type="password"
+          name="password"
+          placeholder="Password"
+          required
+        />
       </Form.Group>
       <Form.Group>
         <Form.Text className="text-danger">
